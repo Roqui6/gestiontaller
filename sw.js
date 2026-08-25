fd6638db578bad748edc9ef9524372a4ed4826e7
Roqui6 <rosermur6@gmail.com>
2026-08-25 11:48:09 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const VERSION = 'v4';
+const CACHE = 'gestiontaller-' + VERSION;
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['GestionTaller.html', 'index.html']))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // Siempre intenta red primero, cae a caché si no hay conexión
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
